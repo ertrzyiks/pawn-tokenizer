5c935122f2c2ac855e09c505007a5737d7fc7e26
Mateusz Derks <mateusz.derks@gmail.com>
2014-09-18 22:49:11 +0200

    Added tests for states and tags

---
+++ test/tags/tags.sma
@@ -0,0 +1,1 @@
+new Float:fTime;
--- test/tokenize.js
+++ test/tokenize.js
@@ -24,59 +24,62 @@ function parse(sourceFile, tokensFile, done) {
 }
     
 describe('Pawn tokenizer', function () {
-    describe('Recognizing comments', function () {
-        it('should recognize one line comments', function (done) {
-            parse('comments/comments.sma', './comments/comments.json',  function (err, seen, expected) {
+    it('should recognize multiline comments', function (done) {
+        parse(
+            'comments/comments_multiline.sma', 
+            './comments/comments_multiline.json',  
+            function (err, seen, expected) {
                 expect(seen).to.deep.equal(expected);
                 done();
-            });
-        });
-        
-        it('should recognize multiline comments', function (done) {
-            parse(
-                'comments/comments_multiline.sma', 
-                './comments/comments_multiline.json',  
-                function (err, seen, expected) {
-                    expect(seen).to.deep.equal(expected);
-                    done();
-                }
-            );
+            }
+        );
+    });
+    it('should recognize different directives', function (done) {
+        parse('directives/directives.sma', './directives/directives.json',  function (err, seen, expected) {
+            expect(seen).to.deep.equal(expected);
+            done();
         });
     });
     
-    describe('Recognizing directives', function () {
-        it('should recognize different directives', function (done) {
-            parse('directives/directives.sma', './directives/directives.json',  function (err, seen, expected) {
-                expect(seen).to.deep.equal(expected);
-                done();
-            });
+    it('should recognize strings with default ctrlchar', function (done) {
+        parse('strings/strings.sma', './strings/strings.json',  function (err, seen, expected) {
+            expect(seen).to.deep.equal(expected);
+            done();
         });
     });
     
-    describe('Recognizing strings', function () {
-        it('should recognize strings with default ctrlchar', function (done) {
-            parse('strings/strings.sma', './strings/strings.json',  function (err, seen, expected) {
-                expect(seen).to.deep.equal(expected);
-                done();
-            });
+    it('should recognize ctrl char change', function (done) {
+        parse('ctrlchar/custom_ctrlchar.sma', './ctrlchar/custom_ctrlchar.json',  function (err, seen, expected) {
+            expect(seen).to.deep.equal(expected);
+            done();
         });
     });
     
-    describe('Changing ctrl char', function () {
-        it('should recognize ctrl char change', function (done) {
-            parse('ctrlchar/custom_ctrlchar.sma', './ctrlchar/custom_ctrlchar.json',  function (err, seen, expected) {
-                expect(seen).to.deep.equal(expected);
-                done();
-            });
+    it('should recognize state', function (done) {
+        parse('states/states.sma', './states/states.json',  function (err, seen, expected) {
+            expect(seen).to.deep.equal(expected);
+            done();
+        });
+    });  
+
+    it('should recognize tags', function (done) {
+        parse('tags/tags.sma', './tags/tags.json',  function (err, seen, expected) {
+            expect(seen).to.deep.equal(expected);
+            done();
+        });
+    });    
+    
+    it('should recognize one line comments', function (done) {
+        parse('comments/comments.sma', './comments/comments.json',  function (err, seen, expected) {
+            expect(seen).to.deep.equal(expected);
+            done();
         });
     });
     
-    describe('Tokenizing', function () {
-        it('should be able to tokenize complex code', function (done) {
-            parse('tokens/tokens.sma', './tokens/tokens.json', function (err, seen, expected) {
-                expect(seen).to.deep.equal(expected);
-                done();
-            });
+    it('should be able to tokenize complex code', function (done) {
+        parse('tokens/tokens.sma', './tokens/tokens.json', function (err, seen, expected) {
+            expect(seen).to.deep.equal(expected);
+            done();
         });
     });
 });
